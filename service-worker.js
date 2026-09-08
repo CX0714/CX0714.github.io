ee8721ace790ee8df608a631f6918bb1b43d7a8e
CX0714 <z120624597@qq.com>
2026-09-08 15:06:35 +0800

    chore: 缓存版本 v2 → v3，让启动画面改动生效

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "philos-v2";
+const CACHE_NAME = "philos-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./icon.svg", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
